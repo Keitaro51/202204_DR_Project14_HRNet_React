0005ec68cc63729eff41e2a64b37d453ee60c476
R.Charlot <romain.charlot@lilo.org>
2022-04-02 17:56:53 +0200

    fix scrolling during modal display - create a full usable pagination and sorting system

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,28 +1,79 @@
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect, useState, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectEmployeeList } from '../utils/selectors'
 
 const List = () => {
-  const data = useSelector(selectEmployeeList)
+  let data = useSelector(selectEmployeeList)
   const [currentPage, setCurrentPage] = useState(1)
   const [nbrOfPages, setNbrOfPages] = useState(null)
-  const tableLength = useRef({ value: 10 })
-
-  useEffect(() => {
-    displayLength()
+  const [sorting, setSorting] = useState({
+    method: 'firstName',
+    direction: 'asc',
   })
 
-  let nbrOfPageButtons = new Array(nbrOfPages).fill(0)
+  const tableLength = useRef({ value: 10 })
 
-  const minIndex = parseInt(tableLength.current.value) * (currentPage - 1)
-  const maxIndex = currentPage * parseInt(tableLength.current.value) - 1
-  let sortData = data.filter(
-    (_employee, index) => index >= minIndex && index <= maxIndex
+  /**
+   * Determine number of pages necessary to all datas
+   */
+  const pagesNumberDetermination = useCallback(() => {
+    setNbrOfPages(Math.ceil(data.length / parseInt(tableLength.current.value)))
+  }, [data.length])
+
+  // create an array with same length than page number, for mapping the right number of buttons
+  let nbrOfPageButtons = new Array(nbrOfPages).fill(undefined)
+
+  useEffect(() => {
+    pagesNumberDetermination()
+  }, [pagesNumberDetermination])
+
+  // determine min and max indexes of data array depending on page size
+  const minDataIndex = parseInt(tableLength.current.value) * (currentPage - 1)
+  const maxDataIndex = currentPage * parseInt(tableLength.current.value) - 1
+
+  /**
+   * Switch between no sorting, ascendant sorting or descendant sorting
+   * @param {string} sortBy determine method (by firstname, lastname, city, etc...)
+   */
+  const setSortBy = (sortBy) => {
+    if (sorting.method === sortBy) {
+      sorting.direction === 'asc'
+        ? setSorting({ method: sortBy, direction: 'desc' })
+        : setSorting({ method: sortBy, direction: 'asc' })
+    } else {
+      setSorting({ method: sortBy, direction: 'asc' })
+    }
+  }
+  //FIXME need dataCopy l47 54 57 58 or generate error during first list render after employee creation
+  const sort = () => {
+    let dataCopy = [...data].sort((a, b) => {
+      if (sorting.direction === 'asc') {
+        return a[sorting.method].toLowerCase() < b[sorting.method].toLowerCase()
+          ? -1
+          : 1
+      } else {
+        return a[sorting.method].toLowerCase() < b[sorting.method].toLowerCase()
+          ? 1
+          : -1
+      }
+    })
+    return dataCopy
+  }
+
+  let sortedData = sort()
+  let pagedData = sortedData.filter(
+    (_employee, index) => index >= minDataIndex && index <= maxDataIndex
   )
 
-  function displayLength() {
-    setNbrOfPages(Math.ceil(data.length / parseInt(tableLength.current.value)))
+  /**
+   * security for specific case
+   * (e.g. positioning on last page then switch to a lower show select value doesn't render the first page
+   * but en empty page with invalid side informations)
+   */
+  function onChange() {
+    setCurrentPage(1)
+    pagesNumberDetermination()
   }
 
   return (
@@ -32,7 +83,7 @@ const List = () => {
         <label htmlFor="show">
           Show
           <select
-            onChange={displayLength}
+            onChange={onChange}
             name="show"
             id="show"
             required
@@ -50,19 +101,19 @@ const List = () => {
       <table>
         <thead>
           <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Start Date</th>
-            <th>Department</th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip Code</th>
+            <th onClick={() => setSortBy('firstName')}>First Name</th>
+            <th onClick={() => setSortBy('lastName')}>Last Name</th>
+            <th onClick={() => setSortBy('startDate')}>Start Date</th>
+            <th onClick={() => setSortBy('department')}>Department</th>
+            <th onClick={() => setSortBy('dateOfBirth')}>Date of Birth</th>
+            <th onClick={() => setSortBy('street')}>Street</th>
+            <th onClick={() => setSortBy('city')}>City</th>
+            <th onClick={() => setSortBy('state')}>State</th>
+            <th onClick={() => setSortBy('zipCode')}>Zip Code</th>
           </tr>
         </thead>
         <tbody>
-          {sortData.map((employee) => (
+          {pagedData.map((employee) => (
             <tr key={employee.id}>
               <td>{employee.firstName}</td>
               <td>{employee.lastName}</td>
@@ -79,9 +130,9 @@ const List = () => {
       </table>
       <div className="pagination">
         <p>
-          Showing {minIndex + 1} to
+          Showing {minDataIndex + 1} to
           {(currentPage - 1) * tableLength.current.value +
-            sortData.length} of {data.length}
+            pagedData.length} of {data.length}
           entries
         </p>
 
